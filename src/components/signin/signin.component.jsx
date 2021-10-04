d95d6f173ea8a127e2c46a72ed0f231b4b5ff9af
suhaybcabdulahi <sabkacade40@gmail.com>
2021-10-04 22:43:47 +0530

    Adding signing component with a custombutton component

---
+++ src/components/signin/signin.component.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './signin.style.scss'
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+class Signin extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    sumitHandle = e => {
+        e.preventDefault();
+        this.setState({email: '', password: ''})
+    }
+    handleChange = e => {
+        const {name, value} = e.target;
+        this.setState({[name]: value})
+    }
+
+    render() {
+        return (
+            <div className="sign-in">
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.sumitHandle}>
+                    <FormInput 
+                    type="email" 
+                    name="email" 
+                    onChange={this.handleChange} 
+                    label="Email"
+                    value={this.state.email} 
+                    required />
+                    
+                    <FormInput 
+                    type="password" 
+                    name="password" 
+                    label="Password"
+                    onChange={this.handleChange} 
+                    value={this.state.password} 
+                    required />
+                    
+
+                    <CustomButton type="submit">SIGN IN</CustomButton>
+                </form>
+
+            </div>
+        )
+    }
+}
+
+export default Signin;
---
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './sign-in-up.style.scss'
+import Signin from '../../components/signin/signin.component';
+
+const Signinup = () => (
+    <div className="sign-in-up">
+        <Signin />
+    </div>
+)
+
+
+
+export default Signinup;
